0435fc61320dbcc716c4cdeb084b81b373344d11
Bastiaan van den Berg <bastiaan@bd.(none)>
2013-04-14 11:25:09 +0200

    Added the header menu to documentation page.

--- spicaweb/doc/build/html/searchindex.js
+++ spicaweb/doc/build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},terms:{index:0,search:0,document:0,welcom:0,modul:0,indic:0,content:0,tabl:0,spica:0,page:0},objtypes:{},titles:["Welcome to SPiCa&#8217;s documentation!"],objnames:{},filenames:["index"]})
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to SPiCa&#8217;s documentation!"],objects:{},titleterms:{welcom:0,spica:0,document:0,indic:0,tabl:0}})
