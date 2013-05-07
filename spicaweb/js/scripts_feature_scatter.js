47cb7029ebbb6e2b82c06632e6d6bc96e5d7fb17
Bastiaan van den Berg <b.a.vandenberg@gmail.com>
2013-05-07 16:54:06 +0200

    Updated show scatter, showing error msg

--- spicaweb/js/scripts_feature_scatter.js
+++ spicaweb/js/scripts_feature_scatter.js
@@ -6,17 +6,27 @@ var max_num_features = Infinity;
 
 function updateContent(labeling_name, class_ids, feat_ids, changed_classes, changed_features) {
 
-    // show loading animation 
-    // TODO get url from setting
-    $("div#scatter img").replaceWith($('<img>').attr({'id': 'loading','src': '/spica/img/load.gif'}))
-
     if(feat_ids.length != 2) {
-        $("div#scatter img").replaceWith('<img>') // TODO print msg
-    }
-    else if(class_ids.length < 1) {
-        $("div#scatter img").replaceWith('<img>') // TODO print msg
+        $("div#feat_error").remove()
+        $("div#scatter").before(
+            '<div id="feat_error" class="alert alert-info alert-error fade in"><p><strong>Info: </strong>Select exactly two features using the feature filter in the right panel.</p></div>'
+        );
     }
     else {
+        $("div#feat_error").remove()
+    }
+
+    if(class_ids.length < 1) {
+        $("div#class_error").remove()
+        $("div#scatter").before(
+            '<div id="class_error" class="alert alert-info alert-error fade in"><p><strong>Info: </strong>Select one or more labels using the label filter in the right panel</p></div>'
+        );
+    }
+    else {
+        $("div#class_error").remove()
+    }
+
+    if(feat_ids.length == 2 && class_ids.length > 0) {
         $("div#scatter img").replaceWith(
             $('<img>')
                 .attr({'src':'ascatter?feat_ids=' + feat_ids + 
@@ -24,22 +34,6 @@ function updateContent(labeling_name, class_ids, feat_ids, changed_classes, chan
                                     '&class_ids=' + class_ids + 
                                     '&figtype=png'})
                 .load(function() {
-                    $("img#loading").remove()
-                    $("button#save").remove()
-                    $("div#scatter")
-                        .append (
-                            $('<button>')
-                                .html('svg')
-                                .button({icons: {primary: "ui-icon-disk"}})
-                                .attr('id', 'save')
-                                .bind('click', function() {
-                                    url = 'ascatter?feature_id=' + feat_ids + 
-                                                 '&labeling_name=' + labeling_name + 
-                                                 '&labels=' + labels_str + 
-                                                 '&figtype=svg';
-                                    window.location.href=url;
-                                })
-                        )
                 })
             )
         return false;
