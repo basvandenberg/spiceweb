8eded69c90b81d9caf2b7035cf6143bd7ba13092
Bastiaan van den Berg <b.a.vandenberg@gmail.com>
2013-05-08 12:00:40 +0200

    Changed info texts, cleaned up some code

--- spicaweb/js/scripts_feature_list.js
+++ spicaweb/js/scripts_feature_list.js
@@ -1,34 +1,13 @@
 $(document).ready(function() {
 
-  $('a.calc').bind('click', function(event) {
-    $.post('calcfeat', 
-           {featvec: $(this).attr('id')}, 
-           function() {
-               location.reload();
-           });
-    return false;
-  });
-
-  setTimeout("location.reload();", 60000);
-
-    // 
-    $("form#upload-custom-features>button").button();
-   
-    // check form input, before submit
-    $("form#upload-custom-features").submit(function(){
-
-        var isFormValid = true;
-
-        var object_ids_f = $("span.fileupload-preview").html();
-        if(fasta_file == '') {
-            form_alert("create-new-project", "No fasta file selected");
-            isFormValid = false;
-        }
-        if(isFormValid) {
-            $("form#create-new-project>button").button('loading');
-        }
-
-        return isFormValid;
+    // bind feature calculation ajax calls to links
+    $('a.calc').bind('click', function(event) {
+        $.post('calcfeat', {featvec: $(this).attr('id')}, function() {
+            location.reload();
+        });
+        return false;
     });
+
+    setTimeout("location.reload();", 10000);
 });
 
