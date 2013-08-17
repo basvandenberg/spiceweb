3b4b1dfbafdab1ba4c87fae3a65716f226aaacbf
basvandenberg <b.a.vandenberg@gmail.com>
2013-08-17 11:33:49 +0200

    Enable running different job types in parallel

--- spiceweb/job_runner/job_runner.py
+++ spiceweb/job_runner/job_runner.py
@@ -6,78 +6,128 @@ import time
 import glob
 import shutil
 import subprocess
+import argparse
 from daemon import Daemon
 from operator import itemgetter
 
-# define project directory location
-# TODO add a set_root_dir function, or pass spica as parameter...
-project_dir = '/home/bastiaan/Develop/spiceweb_test/projects'
+job_types = ['featext', 'classification', 'classify']
+max_num_jobs = [1, 1, 1]
+
+# maximal allowed number of jobs per job type
+max_jobs = dict(zip(job_types, max_num_jobs))
+
+# this is set based on the provided arguments
+project_dir = None
 
 # define log output files
-log_dir = '/home/bastiaan/Develop/spiceweb/spiceweb/job_runner'
+log_dir = os.path.dirname(os.path.abspath(__file__))
 pid_f = os.path.join(log_dir, 'daemon.pid')
+stdout_f = os.path.join(log_dir, 'out.txt')
+stderr_f = os.path.join(log_dir, 'err.txt')
 
 # pause between checking for jobs in queue
 sleep_interval = 5
 
+class JobQueueManager(Daemon):
 
-class JobRunner(Daemon):
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null',
+                 stderr='/dev/null'):
+
+        super(JobQueueManager, self).__init__(pidfile, stdin, stdout, stderr)
+
+        # somehow a print is required here, to get output for the stdout...
+        # HACK, don't remove!
+        print('')
+
+        # list of running jobs per job type, to keep track of all running jobs
+        self.running_jobs = dict(zip(
+            job_types, [[] for i in xrange(len(job_types))]))
+
 
     def run(self):
+
         while True:
 
-            # obtain job list (jobf, project_dir, timestamp)
-            jobs = []
+            # remove finished jobs from running list
+            for job_type in job_types:
+                for job_tuple in self.running_jobs[job_type]:
+
+                    job, run_f, done_f, err_f, fout, ferr = job_tuple
+
+                    # close the log files
+                    fout.close()
+                    ferr.close()
+
+                    # check if it is finished
+                    job_status = job.poll()
+                    if not(job.poll() is None):
+
+                        # remove the daemon from the list
+                        self.running_jobs[job_type].remove(job_tuple)
+
+                        if(job_status == 0):
+                            shutil.move(run_f, done_f)
+                        else:
+                            shutil.move(run_f, err_f)
+
+            # obtain job lists
+            # {jobtype: [(project_dir, job_f, cmd, t, stdout_f, stderr_f)]}
+            jobs = dict(zip(job_types, [[] for i in xrange(len(job_types))]))
+
             for project_d in glob.glob(os.path.join(project_dir, '*', '*')):
 
                 # check for files in the waiting directories
                 wait_f = os.path.join(project_d, 'jobs', 'waiting', '*')
                 for f in glob.glob(wait_f):
                     job_f = os.path.basename(f)
-                    timestamp = int(''.join(job_f.split('_')[:2]))
+                    t = int(''.join(job_f.split('_')[:2]))
                     with open(f, 'r') as fin:
                         cmd = '%s' % (fin.readline())
-                        stdout_f = fin.readline().strip()
-                        stderr_f = fin.readline().strip()
-                    jobs.append((project_d, job_f, cmd, timestamp))
+                        job_type = cmd.split()[0]
+                        job_stdout_f = fin.readline().strip()
+                        job_stderr_f = fin.readline().strip()
 
-            # if found, run the one that was submitted first
-            if(len(jobs) > 0):
+                    jobs.setdefault(job_type, [])\
+                        .append((project_d, job_f, cmd, t,
+                                 job_stdout_f, job_stderr_f))
 
-                # sort by submitted time
-                (project_d, job_f, cmd, t) = sorted(jobs, key=itemgetter(3))[0]
+            # for each job type
+            for job_type in job_types:
 
-                # define file paths
-                wait_f = os.path.join(project_d, 'jobs', 'waiting', job_f)
-                run_f = os.path.join(project_d, 'jobs', 'running', job_f)
-                done_d = os.path.join(project_d, 'jobs', 'done')
-                done_f = os.path.join(done_d, job_f)
-                err_d = os.path.join(project_d, 'jobs', 'error')
-                err_f = os.path.join(err_d, job_f)
+                # check if max number of jobs are running for this job type
+                if(len(jobs[job_type]) > 0 and
+                   len(self.running_jobs[job_type]) < max_jobs[job_type]):
 
-                # move job file to running dir
-                shutil.move(wait_f, run_f)
+                    # sort jobs current job type by timestamp and select first
+                    first_job = sorted(jobs[job_type], key=itemgetter(3))[0]
+                    project_d, job_f, cmd, _, jobout_f, joberr_f = first_job
 
-                # redirect standard file descriptors
-                sys.stdout.flush()
-                sys.stderr.flush()
-                self.stdout = stdout_f
-                self.stderr = stderr_f
-                so = file(self.stdout, 'a+')
-                se = file(self.stderr, 'a+', 0)
-                os.dup2(so.fileno(), sys.stdout.fileno())
-                os.dup2(se.fileno(), sys.stderr.fileno())
+                    # define file paths
+                    wait_f = os.path.join(project_d, 'jobs', 'waiting', job_f)
+                    run_f = os.path.join(project_d, 'jobs', 'running', job_f)
+                    done_f = os.path.join(project_d, 'jobs', 'done', job_f)
+                    err_f = os.path.join(project_d, 'jobs', 'error', job_f)
 
-                # run the job (in a later stage thread the job...? Popen)
-                retcode = subprocess.call(cmd.split())
+                    fout = open(jobout_f, 'w')
+                    ferr = open(joberr_f, 'w')
+                    job = subprocess.Popen(cmd, shell=True, stdout=fout,
+                                           stderr=ferr)
 
-                # move running to either done or error based on return code
-                if(retcode == 0):
-                    shutil.move(run_f, done_f)
-                else:
-                    shutil.move(run_f, err_f)
+                    # move job file from waiting to running
+                    shutil.move(wait_f, run_f)
 
-                # move results...
+                    # add job to running jobs list
+                    self.running_jobs[job_type].append((job, run_f, done_f, err_f, fout, ferr))
+
+            '''
+            print
+            print 'Queue:'
+            print jobs 
+            print
+            print 'Running:'
+            print self.running_jobs
+            print
+            '''
 
             # then sleep for a while before going to the next loop
             time.sleep(sleep_interval)
@@ -85,21 +135,31 @@ class JobRunner(Daemon):
 
 if __name__ == "__main__":
 
+    actions = ['start', 'stop', 'restart']
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--project_dir', required=True)
+    parser.add_argument('-a', '--action', choices=actions, required=True)
+
+    args = parser.parse_args()
+
+    # set path to projects dir
+    if(os.path.exists(args.project_dir)):
+        project_dir = args.project_dir
+    else:
+        print('Provided projects dir does not exist.')
+        print(args.project_dir)
+        sys.exit(2)
+
     # create the daemon
-    daemon = JobRunner(pid_f)
+    daemon = JobQueueManager(pid_f, stdout=stdout_f, stderr=stderr_f)
 
     # start, stop, or restart the jobrunner daemon
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        else:
-            print "Unknown command"
-            sys.exit(2)
-        sys.exit(0)
+    if(args.action == 'start'):
+        daemon.start()
+    elif(args.action == 'stop'):
+        daemon.stop()
+    elif(args.action == 'restart'):
+        daemon.restart()
     else:
-        print "usage: %s start|stop|restart" % sys.argv[0]
-        sys.exit(2)
+        print("That's weird, this should not be possible...")
